82eaf64b28247b53328a54bee1e457c025dac0f3
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2021-03-21 21:22:01 -0300

    Anime detail template

---
+++ AhoBox/exemplos/estruraAnime.py
@@ -0,0 +1,248 @@
+a = {
+    'request_hash': 'request:anime:e09f711f9e532dc4ffb4d5649b5dbd6e0fb25648',
+    'request_cached': True,
+    'request_cache_expiry': 62129,
+    'mal_id': 11061,
+    'url': 'https://myanimelist.net/anime/11061/Hunter_x_Hunter_2011',
+    'image_url': 'https://cdn.myanimelist.net/images/anime/11/33657.jpg',
+    'trailer_url': 'https://www.youtube.com/embed/D9iTQRB4XRk?enablejsapi=1&wmode=opaque&autoplay=1',
+    'title': 'Hunter x Hunter (2011)',
+    'title_english': 'Hunter x Hunter',
+    'title_japanese': 'HUNTER×HUNTER（ハンター×ハンター）',
+    'title_synonyms': ['HxH (2011)'],
+    'type': 'TV',
+    'source': 'Manga',
+    'episodes': 148,
+    'status': 'Finished Airing',
+    'airing': False,
+    'aired': {
+        'from': '2011-10-02T00:00:00+00:00',
+        'to': '2014-09-24T00:00:00+00:00',
+        'prop': {
+            'from': {
+                'day': 2,
+                'month': 10,
+                'year': 2011
+            },
+            'to': {
+                'day': 24,
+                'month': 9,
+                'year': 2014
+            }
+        },
+        'string': 'Oct 2, 2011 to Sep 24, 2014'
+    },
+    'duration': '23 min per ep',
+    'rating': 'PG-13 - Teens 13 or older',
+    'score': 9.09,
+    'scored_by': 1018632,
+    'rank': 6,
+    'popularity': 12,
+    'members': 1758428,
+    'favorites': 153888,
+    'synopsis': 'Hunter x Hunter is set in a world where Hunters exist to perform all manner of dangerous tasks like capturing criminals and bravely searching for lost treasures in uncharted territories. Twelve-year-old Gon Freecss is determined to become the best Hunter possible in hopes of finding his father, who was a Hunter himself and had long ago abandoned his young son. However, Gon soon realizes the path to achieving his goals is far more challenging than he could have ever imagined. Along the way to becoming an official Hunter, Gon befriends the lively doctor-in-training Leorio, vengeful Kurapika, and rebellious ex-assassin Killua. To attain their own goals and desires, together the four of them take the Hunter Exam, notorious for its low success rate and high probability of death. Throughout their journey, Gon and his friends embark on an adventure that puts them through many hardships and struggles. They will meet a plethora of monsters, creatures, and characters—all while learning what being a Hunter truly means. [Written by MAL Rewrite]',
+    'background': None,
+    'premiered': 'Fall 2011',
+    'broadcast': 'Sundays at 10:55 (JST)',
+    'related': {
+        'Adaptation': [{
+            'mal_id': 26,
+            'type': 'manga',
+            'name': 'Hunter x Hunter',
+            'url': 'https://myanimelist.net/manga/26/Hunter_x_Hunter'
+        }],
+        'Alternative version': [{
+            'mal_id': 136,
+            'type': 'anime',
+            'name': 'Hunter x Hunter',
+            'url': 'https://myanimelist.net/anime/136/Hunter_x_Hunter'
+        }, {
+            'mal_id': 137,
+            'type': 'anime',
+            'name': 'Hunter x Hunter: Original Video Animation',
+            'url': 'https://myanimelist.net/anime/137/Hunter_x_Hunter__Original_Video_Animation'
+        }, {
+            'mal_id': 138,
+            'type': 'anime',
+            'name': 'Hunter x Hunter: Greed Island',
+            'url': 'https://myanimelist.net/anime/138/Hunter_x_Hunter__Greed_Island'
+        }, {
+            'mal_id': 139,
+            'type': 'anime',
+            'name': 'Hunter x Hunter: Greed Island Final',
+            'url': 'https://myanimelist.net/anime/139/Hunter_x_Hunter__Greed_Island_Final'
+        }],
+        'Side story': [{
+            'mal_id': 13271,
+            'type': 'anime',
+            'name': 'Hunter x Hunter Movie 1: Phantom Rouge',
+            'url': 'https://myanimelist.net/anime/13271/Hunter_x_Hunter_Movie_1__Phantom_Rouge'
+        }, {
+            'mal_id': 19951,
+            'type': 'anime',
+            'name': 'Hunter x Hunter Movie 2: The Last Mission',
+            'url': 'https://myanimelist.net/anime/19951/Hunter_x_Hunter_Movie_2__The_Last_Mission'
+        }]
+    },
+    'producers': [{
+        'mal_id': 29,
+        'type': 'anime',
+        'name': 'VAP',
+        'url': 'https://myanimelist.net/anime/producer/29/VAP'
+    }, {
+        'mal_id': 1003,
+        'type': 'anime',
+        'name': 'Nippon Television Network',
+        'url': 'https://myanimelist.net/anime/producer/1003/Nippon_Television_Network'
+    }, {
+        'mal_id': 1365,
+        'type': 'anime',
+        'name': 'Shueisha',
+        'url': 'https://myanimelist.net/anime/producer/1365/Shueisha'
+    }],
+    'licensors': [{
+        'mal_id': 119,
+        'type': 'anime',
+        'name': 'VIZ Media',
+        'url': 'https://myanimelist.net/anime/producer/119/VIZ_Media'
+    }],
+    'studios': [{
+        'mal_id': 11,
+        'type': 'anime',
+        'name': 'Madhouse',
+        'url': 'https://myanimelist.net/anime/producer/11/Madhouse'
+    }],
+    'genres': [{
+        'mal_id': 1,
+        'type': 'anime',
+        'name': 'Action',
+        'url': 'https://myanimelist.net/anime/genre/1/Action'
+    }, {
+        'mal_id': 2,
+        'type': 'anime',
+        'name': 'Adventure',
+        'url': 'https://myanimelist.net/anime/genre/2/Adventure'
+    }, {
+        'mal_id': 10,
+        'type': 'anime',
+        'name': 'Fantasy',
+        'url': 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }, {
+        'mal_id': 27,
+        'type': 'anime',
+        'name': 'Shounen',
+        'url': 'https://myanimelist.net/anime/genre/27/Shounen'
+    }, {
+        'mal_id': 31,
+        'type': 'anime',
+        'name': 'Super Power',
+        'url': 'https://myanimelist.net/anime/genre/31/Super_Power'
+    }],
+    'opening_themes': ['#1: "departure!" by Ono Masatoshi (eps 1-26, 50-52, 62-75, 137-147)',
+                       '#2: "departure! -second version-" by Ono Masatoshi (eps 27-49, 76-103, 109-134, 136)',
+                       '#3: "departure! -Opening Tokubetsu-hen-" by Ono Masatoshi (eps 53-61, 104-108)'],
+    'ending_themes': ['#1: "Just Awake" by Fear, and Loathing in Las Vegas (eps 1-26)',
+                      '#2: "HUNTING FOR YOUR DREAM" by Galneryus (eps 27-50, 52-58)',
+                      '#3: "Riot" by Yoshihisa Hirano (ep 51)', '#4: "REASON" by YUZU (ゆず) (eps 59-75, 147)',
+                      '#5: "Nagareboshi Kirari (YUZU Version) (流れ星キラリ (ゆずバージョン))" by YUZU (ゆず) (eps 76-98)',
+                      '#6: "Hyouriittai (表裏一体)" by YUZU (ゆず) (eps 99-134, 136)',
+                      '#7: "Understanding" by Yoshihisa Hirano (ep 135)',
+                      '#8: "Hyouriittai -second version- (表裏一体)" by YUZU (ゆず) (eps 137-146)',
+                      '#9: "departure!" by Ono Masatoshi (ep 148)'],
+    'jikan_url': 'https://api.jikan.moe/v3/anime/11061',
+    'headers': {
+        'Server': 'nginx/1.18.0 (Ubuntu)',
+        'Date': 'Sun, 21 Mar 2021 17:53:10 GMT',
+        'Content-Type': 'application/json',
+        'Content-Length': '2142',
+        'Connection': 'keep-alive',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Cache-Control': 'private, must-revalidate',
+        'ETag': '"671d0f46c333ce5fb09f6974dd9392d6"',
+        'X-Request-Hash': 'request:anime:e09f711f9e532dc4ffb4d5649b5dbd6e0fb25648',
+        'X-Request-Cached': '1',
+        'X-Request-Cache-Ttl': '62129',
+        'Expires': 'Mon, 22 Mar 2021 11:08:39 GMT',
+        'Content-Encoding': 'gzip',
+        'Vary': 'Accept-Encoding',
+        'X-Cache-Status': 'EXPIRED'
+    }
+}
+
+
+def formataNome():
+    titles = {}
+    for key in a:
+        if 'title' in key:
+            titles[key] = a.get(key)
+
+    genres = []
+    for value in a.get('genres'):
+        genre = {
+            'mal_id': value.get('mal_id'),
+            'name': value.get('name')
+        }
+        genres.append(genre)
+
+    studios = []
+    for value in a.get('studios'):
+        studio = {
+            'mal_id': value.get('mal_id'),
+            'name': value.get('name')
+        }
+        studios.append(studio)
+
+
+    anime = {
+        'mal_id': a.get('mal_id'),
+        'name': a.get('title'),
+        'img_url': a.get('image_url'),
+        'trailer_url': a.get('trailer_url'),
+        'titles': titles,
+        'type': a.get('type'),
+        'score': a.get('score'),
+        'scored_by': a.get('scored_by'),
+        'rank': a.get('rank'),
+        'episodes': a.get('episodes'),
+        'status': a.get('status'),
+        'broadcast': a.get('broadcast'),
+        'genres': genres,
+        'studios': studios,
+        'synopsis': a.get('synopsis')
+
+
+    }
+    print(anime)
+
+
+if __name__ == '__main__':
+    formataNome()
+
+b = {'mal_id': 11061,
+     'name': 'Hunter x Hunter (2011)',
+     'img_url': 'https://cdn.myanimelist.net/images/anime/11/33657.jpg',
+     'trailer_url': 'https://www.youtube.com/embed/D9iTQRB4XRk?enablejsapi=1&wmode=opaque&autoplay=1',
+     'titles': {'title': 'Hunter x Hunter (2011)',
+                'title_english': 'Hunter x Hunter',
+                'title_japanese': 'HUNTER×HUNTER（ハンター×ハンター）',
+                'title_synonyms': ['HxH (2011)']},
+     'type': 'TV',
+     'score': 9.09,
+     'rank': 6,
+     'episodes': 148,
+     'status': 'Finished Airing',
+     'broadcast': 'Sundays at 10:55 (JST)',
+     'genres': [{'mal_id': 1,
+                 'name': 'Action'},
+                {'mal_id': 2,
+                 'name': 'Adventure'},
+                {'mal_id': 10,
+                 'name': 'Fantasy'},
+                {'mal_id': 27,
+                 'name': 'Shounen'},
+                {'mal_id': 31,
+                 'name': 'Super Power'}],
+     'studios': [{'mal_id': 11,
+                  'name': 'Madhouse'}],
+     'synopsis': 'Hunter x Hunter is set in a world where Hunters exist to perform all manner of dangerous tasks like capturing criminals and bravely searching for lost treasures in uncharted territories. Twelve-year-old Gon Freecss is determined to become the best Hunter possible in hopes of finding his father, who was a Hunter himself and had long ago abandoned his young son. However, Gon soon realizes the path to achieving his goals is far more challenging than he could have ever imagined. Along the way to becoming an official Hunter, Gon befriends the lively doctor-in-training Leorio, vengeful Kurapika, and rebellious ex-assassin Killua. To attain their own goals and desires, together the four of them take the Hunter Exam, notorious for its low success rate and high probability of death. Throughout their journey, Gon and his friends embark on an adventure that puts them through many hardships and struggles. They will meet a plethora of monsters, creatures, and characters—all while learning what being a Hunter truly means. [Written by MAL Rewrite]'}
